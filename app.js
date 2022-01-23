2511d79a3e20aa988f56d36ff18f5023ab4ed8d2
Bezll <jabez1992@hotmail.com>
2022-01-23 11:27:50 +0000

    remove swagger-jsDoc

--- app.js
+++ app.js
@@ -1,5 +1,4 @@
 const swaggerUI = require("swagger-ui-express"); //<< generate swagger UI
-const swaggerJsDoc = require("swagger-jsdoc"); // << generate swagger doc automatically using options
 const express = require("express");
 const { getCategories, postCategory } = require("./controllers/categories");
 const { getUsers, getUsersByUsername } = require("./controllers/users");
@@ -23,77 +22,12 @@ const {
 	handleServerErrors,
 } = require("./errors/errors");
 
-const options = {
-	definition: {
-		openapi: "3.0.0",
-		info: {
-			title: "Game API",
-			version: "1.0.0",
-			description: "A simple Express Game API",
-		},
-		servers: [
-			{
-				url: "http://localhost:9090",
-			},
-		],
-	},
-	apis: ["./app.js"], //read from app.js file
-};
-const specs = swaggerJsDoc(options);
+const swaggerDocument = require("./swagger.json");
 
 const app = express();
 app.use(express.json());
 
-app.use("/api", swaggerUI.serve, swaggerUI.setup(specs)); //display swagger api doc
-
-/**
- * @swagger
- * components:
- *   schemas:
- *     Categories:
- *       type: object
- *       required:
- *         - title
- *         - author
- *       properties:
- *         id:
- *           type: string
- *           description: The auto-generated id of the category
- *         slug:
- *           type: string
- *           description: The category slug
- *         description:
- *           type: string
- *           description: The category description
- *       example:
- *         id: 1
- *         title: euro game
- *         author: Abstact games that involve little luck
- */
-
-/**
- * @swagger
- * tags:
- *   name: categories
- *   description: The categories managing API
- */
-
-/**
- * @swagger
- * /categories:
- *   get:
- *     summary: Returns the list of all the categories
- *     tags: [categories]
- *     responses:
- *       200:
- *         description: The list of the categories
- *         content:
- *           application/json:
- *             schema:
- *               type: array
- *               items:
- *                 $ref: '#/components/schemas/Categories'
- */
+app.use("/api", swaggerUI.serve, swaggerUI.setup(swaggerDocument));
 
 app.get("/api/categories", getCategories);
 app.post("/api/categories", postCategory);
